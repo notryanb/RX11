f2e8cd4c4d152f5a6c528911231a7ff2c09bacbf
Ryan Blecher <notryanb@gmail.com>
2025-01-05 14:22:47 -0500

    Use a mono voice in the synth to start testing note-on and note-off instead of a flag

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 // use parking_lot::Mutex;
 
 const MAX_BLOCK_SIZE: usize = 64;
+const MIDI_MAX_VELOCITY: f32 = 127.0;
 
 struct NoiseGenerator {
     noise_seed: u32,
@@ -26,26 +27,47 @@ impl NoiseGenerator {
 struct Synth {
     pub noise_mix: f32,
     noise_gen: NoiseGenerator,
-    pub should_be_playing_note: bool, // TESTING
+    pub voice: Voice,
 }
 
 impl Synth {
     fn new() -> Self {
         Self {
-            noise_mix: 0.09,
+            noise_mix: 0.7,
             noise_gen: NoiseGenerator::new(),
-            should_be_playing_note: false, // TESTING
+            voice: Default::default(),
         }
     }
     fn reset(&mut self) {
         self.noise_gen.reset();
+        self.voice.reset();
+    }
+
+    fn note_on(&mut self, note: u8, velocity: f32) {
+        self.voice.note = note;
+        self.voice.velocity = velocity;
+    }
+
+    fn note_off(&mut self, note: u8) {
+        if self.voice.note == note {
+            self.voice.note = 0;
+            self.voice.velocity = 0.0;
+        }
     }
 
     //fn render(&mut self, output_buffer: &mut Buffer) {
     fn render(&mut self, output_buffer: &mut [&mut [f32]], block_start: usize, block_end: usize) {
         for (value_idx, sample_idx) in (block_start..block_end).enumerate() {
-            output_buffer[0][sample_idx] += self.noise_gen.next_value() * self.noise_mix;
-            output_buffer[1][sample_idx] += self.noise_gen.next_value() * self.noise_mix;
+            let noise = self.noise_gen.next_value();
+            let mut output_sample = 0.0f32;
+
+            if self.voice.note > 0 {
+                output_sample =
+                    noise * (self.voice.velocity as f32 / MIDI_MAX_VELOCITY) * self.noise_mix;
+            }
+
+            output_buffer[0][sample_idx] += output_sample;
+            output_buffer[1][sample_idx] += output_sample;
         }
 
         /*
@@ -60,6 +82,20 @@ impl Synth {
             }
         }
         */
+    }
+}
+
+/// Produces the next output sample for a given note
+#[derive(Default)]
+struct Voice {
+    pub note: u8,
+    pub velocity: f32,
+}
+
+impl Voice {
+    pub fn reset(&mut self) {
+        self.note = 0;
+        self.velocity = 0.0;
     }
 }
 
@@ -189,18 +225,16 @@ impl Plugin for RX11 {
                                 note,
                                 velocity,
                             } => {
-                                self.synth.should_be_playing_note = true;
-                                // Don't do the rendering here, but this should take all of the NoteOn info
-                                // and feed it into the synth's state so it can render later on
+                                self.synth.note_on(note, velocity);
                             }
                             NoteEvent::NoteOff {
                                 timing: _,
                                 voice_id: _,
                                 channel: _,
-                                note: _,
+                                note,
                                 velocity: _,
                             } => {
-                                self.synth.should_be_playing_note = false;
+                                self.synth.note_off(note);
                             }
                             _ => {}
                         }
@@ -219,10 +253,7 @@ impl Plugin for RX11 {
             output[0][block_start..block_end].fill(0.0);
             output[1][block_start..block_end].fill(0.0);
 
-            // naive rendering for testing
-            if self.synth.should_be_playing_note {
-                self.synth.render(output, block_start, block_end);
-            }
+            self.synth.render(output, block_start, block_end);
 
             block_start = block_end;
             block_end = (block_start + MAX_BLOCK_SIZE).min(num_samples);
