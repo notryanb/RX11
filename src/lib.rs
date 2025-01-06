7beba05d1c52a097e2eb187f0f247eda33a958b0
Ryan Blecher <notryanb@gmail.com>
2025-01-05 20:29:18 -0500

    Implement a band-limited impulse train saw tooth oscillator with dc offset

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 
 const MAX_BLOCK_SIZE: usize = 64;
 const MIDI_MAX_VELOCITY: f32 = 127.0;
+const PI_OVER_FOUR: f32 = std::f32::consts::PI / 4.0; //0.7853981633974483;
 
 struct NoiseGenerator {
     noise_seed: u32,
@@ -54,7 +55,7 @@ impl Synth {
 
         // TODO - Expose these as methods on the voice or maybe on the synth itself?
         self.voice.oscillator.amplitude = (velocity / MIDI_MAX_VELOCITY) * 0.7;
-        self.voice.oscillator.increment = frequency / self.sample_rate;
+        self.voice.oscillator.period = self.sample_rate / frequency;
         self.voice.oscillator.reset();
     }
 
@@ -98,6 +99,7 @@ impl Synth {
 struct Voice {
     pub note: i32,
     pub velocity: f32,
+    pub saw: f32,
     pub oscillator: Oscillator,
 }
 
@@ -105,17 +107,22 @@ impl Voice {
     pub fn reset(&mut self) {
         self.note = 0;
         self.velocity = 0.0;
+        self.saw = 0.0;
         self.oscillator.reset();
     }
 
     pub fn render(&mut self) -> f32 {
-        self.oscillator.next_sample()
+        let mut sample = self.oscillator.next_sample();
+        self.saw = self.saw * 0.997 + sample;
+        self.saw
     }
 }
 
 #[derive(Default)]
 struct Oscillator {
     pub amplitude: f32,
+
+    pub period: f32,
 
     /// How fast to move through the phase
     pub increment: f32,
@@ -124,6 +131,10 @@ struct Oscillator {
     /// It is a "modulo counter" aka "phasor"
     pub phase: f32,
 
+    phase_max: f32,
+
+    dc_offset: f32,
+
     sin0: f32,
     sin1: f32,
     dsin: f32,
@@ -131,18 +142,48 @@ struct Oscillator {
 
 impl Oscillator {
     fn reset(&mut self) {
+        self.increment = 0.0;
+        self.dc_offset = 0.0;
         self.phase = 0.0;
-
-        self.sin0 = self.amplitude * (std::f32::consts::TAU * self.phase).sin();
-        self.sin1 = self.amplitude * (std::f32::consts::TAU * (self.phase - self.increment)).sin();
-        self.dsin = 2.0  * (std::f32::consts::TAU * self.increment).cos();
+        self.sin0 = 0.0;
+        self.sin1 = 0.0;
+        self.dsin = 0.0;
     }
 
     fn next_sample(&mut self) -> f32 {
-        let sinx = self.dsin * self.sin0 - self.sin1;
-        self.sin1 = self.sin0;
-        self.sin0 = sinx;
-        sinx
+        let mut output = 0.0;
+        self.phase += self.increment;
+        
+        if self.phase <= PI_OVER_FOUR {
+            let half_period = self.period / 2.0;
+            self.phase_max = (0.5 + half_period).floor() - 0.5; 
+            self.dc_offset = 0.5 * self.amplitude / self.phase_max;
+            self.phase_max *= std::f32::consts::PI;
+            self.increment = self.phase_max / half_period;
+            self.phase = -self.phase;
+
+            self.sin0 = self.amplitude * self.phase.sin();
+            self.sin1 = self.amplitude * (self.phase - self.increment).sin();
+            self.dsin = 2.0 * self.increment.cos();
+
+            if self.phase * self.phase > 1e-9 { 
+                output = self.sin0 / self.phase;
+            } else {
+                output = self.amplitude;
+            }
+        } else {
+            if self.phase > self.phase_max {
+                self.phase = self.phase_max + self.phase_max - self.phase; 
+                self.increment = -self.increment;
+            }
+
+            let sinp = self.dsin * self.sin0 - self.sin1; 
+            self.sin1 = self.sin0;
+            self.sin0 = sinp;
+            output = sinp / self.phase;
+        }
+        
+        output - self.dc_offset
     }
 }
 
