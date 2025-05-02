e78360d04c265f57b976348301766ca6fb418b25
Ryan Blecher <notryanb@gmail.com>
2025-05-02 17:59:30 -0400

    Some UI refactor

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use nih_plug::midi::control_change::{
     ALL_NOTES_OFF, ALL_SOUND_OFF, MAIN_VOLUME_MSB, MODULATION_MSB,
 };
 use nih_plug::prelude::*;
-use nih_plug_egui::{create_egui_editor, egui, widgets, EguiState};
+use nih_plug_egui::{create_egui_editor, egui, EguiState};
 use tracing_subscriber::prelude::*;
 
 use std::sync::Arc;
@@ -14,11 +14,13 @@ mod oscillator;
 mod presets;
 mod state_variable_filter;
 mod synth;
+mod ui;
 mod voice;
 
 use crate::presets::{Preset, Presets};
 use crate::synth::Synth;
 use crate::logger::EventCollector;
+use crate::ui::{rx11_egui_ui, UiView};
 
 const MAX_BLOCK_SIZE: usize = 64;
 
@@ -81,6 +83,8 @@ impl GlideMode {
 pub struct UiState {
     pub selected_preset: String,
     pub loaded_preset_on_startup: bool,
+    pub _current_view: UiView,
+    pub show_debug: bool,
 }
 
 pub struct RX11 {
@@ -102,7 +106,12 @@ impl Default for RX11 {
             params: Arc::new(RX11Params::default()),
             synth: Synth::new(),
             presets: Presets::init(),
-            ui_state: UiState { selected_preset: "Init".into(), loaded_preset_on_startup: false },
+            ui_state: UiState { 
+                selected_preset: "Init".into(), 
+                loaded_preset_on_startup: false,
+                _current_view: UiView::Synth,
+                show_debug: false,
+            },
             logs: collector,
         }
     }
@@ -198,7 +207,7 @@ pub struct RX11Params {
 impl Default for RX11Params {
     fn default() -> Self {
         Self {
-            editor_state: EguiState::from_size(800, 600),
+            editor_state: EguiState::from_size(80, 600),
 
             osc_mix: FloatParam::new(
                 "Osc Mix",
@@ -505,59 +514,6 @@ impl Default for RX11Params {
     }
 }
 
-pub fn load_preset(preset: &Preset, setter: &ParamSetter, params: &RX11Params) {
-    for (param_name, param_value) in &preset.values {
-        if &param_name[..] == "glide_mode" {
-            setter.begin_set_parameter(&params.glide_mode);
-            setter.set_parameter(
-                &params.glide_mode,
-                GlideMode::from_f32(*param_value),
-            );
-            setter.end_set_parameter(&params.glide_mode);
-        } else if &param_name[..] == "poly_mode" {
-            setter.begin_set_parameter(&params.poly_mode);
-            setter.set_parameter(
-                &params.poly_mode,
-                PolyMode::from_f32(*param_value),
-            );
-            setter.end_set_parameter(&params.poly_mode);
-        } else {
-            let param = match &param_name[..] {
-                "osc_mix" => Some(&params.osc_mix),
-                "osc_tune" => Some(&params.osc_tune),
-                "osc_fine_tune" => Some(&params.osc_fine_tune),
-                "glide_rate" => Some(&params.glide_rate),
-                "glide_bend" => Some(&params.glide_bend),
-                "filter_freq" => Some(&params.filter_freq),
-                "filter_reso" => Some(&params.filter_reso),
-                "filter_env" => Some(&params.filter_env),
-                "filter_lfo" => Some(&params.filter_lfo),
-                "filter_velocity" => Some(&params.filter_velocity),
-                "filter_attack" => Some(&params.filter_attack),
-                "filter_decay" => Some(&params.filter_decay),
-                "filter_sustain" => Some(&params.filter_sustain),
-                "filter_release" => Some(&params.filter_release),
-                "env_attack" => Some(&params.env_attack),
-                "env_decay" => Some(&params.env_decay),
-                "env_sustain" => Some(&params.env_sustain),
-                "env_release" => Some(&params.env_release),
-                "lfo_rate" => Some(&params.lfo_rate),
-                "vibrato" => Some(&params.vibrato),
-                "noise" => Some(&params.noise_level),
-                "octave" => Some(&params.octave),
-                "tuning" => Some(&params.tuning),
-                "output" => Some(&params.output_level),
-                _ => None,
-            };
-
-            if let Some(param) = param {
-                setter.begin_set_parameter(param);
-                setter.set_parameter(param, *param_value);
-                setter.end_set_parameter(param);
-            }
-        }
-    }
-}
 
 impl Plugin for RX11 {
     const NAME: &'static str = "RX11 Synth";
@@ -602,228 +558,14 @@ impl Plugin for RX11 {
         let params = self.params.clone();
         let presets = self.presets.clone();
         let logger = self.logs.clone();
+        let egui_state = params.editor_state.clone();
 
         create_egui_editor(
             self.params.editor_state.clone(),
             self.ui_state.clone(),
             |_, _| {},
             move |egui_ctx, setter, state| {
-                let UiState { selected_preset, loaded_preset_on_startup } = state;
-
-                if !*loaded_preset_on_startup {
-                    if let Some(preset) = presets.0.first() {
-                        load_preset(&preset, &setter, &params);
-                        *loaded_preset_on_startup = true;
-                    } else {
-                        tracing::debug!("There was no preset to load on startup");
-                    }
-                }
-
-                egui::Window::new("Logs").min_width(400.0).show(egui_ctx, |ui| {
-                    ui.label("Logs go under here");
-                    ui.separator();
-                    
-                    egui::ScrollArea::vertical()
-                        .scroll_bar_visibility(egui::containers::scroll_area::ScrollBarVisibility::AlwaysVisible)
-                        .show(ui, |ui| {
-                            for evt in logger.events().iter() {
-                                if let Some(field) = evt.fields.first_key_value() {
-                                    ui.label(field.1);
-                                }
-                            }
-                        });
-                });
-                
-                egui::TopBottomPanel::top("menu").show(egui_ctx, |ui| {
-                    ui.horizontal(|ui| {
-                        // Displaying / selecting the presets
-                        ui.menu_button("Presets", |ui| {
-                            egui::ScrollArea::vertical()
-                                .scroll_bar_visibility(
-                                    egui::containers::scroll_area::ScrollBarVisibility::VisibleWhenNeeded,
-                                )
-                                .show(ui, |ui| {
-                                    for preset in &presets.0 {
-                                        if ui.button(&preset.name).clicked() {
-                                            *selected_preset = preset.name.clone();
-                                            load_preset(&preset, &setter, &params);
-                                            // How do I close the dropdown on button click?
-                                        }
-                                    }
-                                })
-                        });
-                        ui.label(format!("Preset: {}", selected_preset));
-                    })
-                });
-                egui::CentralPanel::default().show(egui_ctx, |ui| {
-                    egui::ScrollArea::vertical()
-                        .scroll_bar_visibility(
-                            egui::containers::scroll_area::ScrollBarVisibility::AlwaysVisible,
-                        )
-                        .show(ui, |ui| {
-                            let glide_mode = &params.glide_mode.value();
-                            ui.horizontal(|ui| {
-                                if ui
-                                    .add(egui::widgets::SelectableLabel::new(
-                                        *glide_mode == GlideMode::Off,
-                                        "Glide Off",
-                                    ))
-                                    .clicked()
-                                {
-                                    setter.begin_set_parameter(&params.glide_mode);
-                                    setter.set_parameter(&params.glide_mode, GlideMode::Off);
-                                    setter.end_set_parameter(&params.glide_mode);
-                                }
-                                if ui
-                                    .add(egui::widgets::SelectableLabel::new(
-                                        *glide_mode == GlideMode::Legato,
-                                        "Legato",
-                                    ))
-                                    .clicked()
-                                {
-                                    setter.begin_set_parameter(&params.glide_mode);
-                                    setter.set_parameter(&params.glide_mode, GlideMode::Legato);
-                                    setter.end_set_parameter(&params.glide_mode);
-                                }
-                                if ui
-                                    .add(egui::widgets::SelectableLabel::new(
-                                        *glide_mode == GlideMode::Always,
-                                        "Glide Always",
-                                    ))
-                                    .clicked()
-                                {
-                                    setter.begin_set_parameter(&params.glide_mode);
-                                    setter.set_parameter(&params.glide_mode, GlideMode::Always);
-                                    setter.end_set_parameter(&params.glide_mode);
-                                }
-                            });
-                            ui.end_row();
-
-                            ui.separator();
-
-                            let poly_mode = &params.poly_mode.value();
-                            ui.horizontal(|ui| {
-                                if ui
-                                    .add(egui::widgets::SelectableLabel::new(
-                                        *poly_mode == PolyMode::Mono,
-                                        "Mono",
-                                    ))
-                                    .clicked()
-                                {
-                                    setter.begin_set_parameter(&params.poly_mode);
-                                    setter.set_parameter(&params.poly_mode, PolyMode::Mono);
-                                    setter.end_set_parameter(&params.poly_mode);
-                                }
-                                if ui
-                                    .add(egui::widgets::SelectableLabel::new(
-                                        *poly_mode == PolyMode::Poly,
-                                        "Poly",
-                                    ))
-                                    .clicked()
-                                {
-                                    setter.begin_set_parameter(&params.poly_mode);
-                                    setter.set_parameter(&params.poly_mode, PolyMode::Poly);
-                                    setter.end_set_parameter(&params.poly_mode);
-                                }
-                            });
-                            ui.end_row();
-
-                            ui.separator();
-
-                            ui.label("Oscillator Mix");
-                            ui.add(widgets::ParamSlider::for_param(&params.osc_mix, setter));
-
-                            ui.label("Oscillator Tune");
-                            ui.add(widgets::ParamSlider::for_param(&params.osc_tune, setter));
-
-                            ui.label("Oscillator Fine Tune");
-                            ui.add(widgets::ParamSlider::for_param(
-                                &params.osc_fine_tune,
-                                setter,
-                            ));
-
-                            ui.label("Glide Rate");
-                            ui.add(widgets::ParamSlider::for_param(&params.glide_rate, setter));
-
-                            ui.label("Glide Bend");
-                            ui.add(widgets::ParamSlider::for_param(&params.glide_bend, setter));
-
-                            ui.label("Filter Frequency");
-                            ui.add(widgets::ParamSlider::for_param(&params.filter_freq, setter));
-
-                            ui.label("Filter Resonance");
-                            ui.add(widgets::ParamSlider::for_param(&params.filter_reso, setter));
-
-                            ui.label("Filter LFO");
-                            ui.add(widgets::ParamSlider::for_param(&params.filter_lfo, setter));
-
-                            ui.label("Filter Velocity");
-                            ui.add(widgets::ParamSlider::for_param(
-                                &params.filter_velocity,
-                                setter,
-                            ));
-
-                            ui.label("Filter ADSR");
-                            ui.add(widgets::ParamSlider::for_param(&params.filter_env, setter));
-
-                            ui.label("Filter Attack");
-                            ui.add(widgets::ParamSlider::for_param(
-                                &params.filter_attack,
-                                setter,
-                            ));
-
-                            ui.label("Filter Decay");
-                            ui.add(widgets::ParamSlider::for_param(
-                                &params.filter_decay,
-                                setter,
-                            ));
-
-                            ui.label("Filter Sustain");
-                            ui.add(widgets::ParamSlider::for_param(
-                                &params.filter_sustain,
-                                setter,
-                            ));
-
-                            ui.label("Filter Release");
-                            ui.add(widgets::ParamSlider::for_param(
-                                &params.filter_release,
-                                setter,
-                            ));
-
-                            ui.label("Envelope Attack");
-                            ui.add(widgets::ParamSlider::for_param(&params.env_attack, setter));
-
-                            ui.label("Envelope Decay");
-                            ui.add(widgets::ParamSlider::for_param(&params.env_decay, setter));
-
-                            ui.label("Envelope Sustain");
-                            ui.add(widgets::ParamSlider::for_param(&params.env_sustain, setter));
-
-                            ui.label("Envelope Release");
-                            ui.add(widgets::ParamSlider::for_param(&params.env_release, setter));
-
-                            ui.label("LFO Rate");
-                            ui.add(widgets::ParamSlider::for_param(&params.lfo_rate, setter));
-
-                            ui.label("Vibrato");
-                            ui.add(widgets::ParamSlider::for_param(&params.vibrato, setter));
-
-                            ui.label("Noise");
-                            ui.add(widgets::ParamSlider::for_param(&params.noise_level, setter));
-
-                            ui.label("Octave");
-                            ui.add(widgets::ParamSlider::for_param(&params.octave, setter));
-
-                            ui.label("Tuning");
-                            ui.add(widgets::ParamSlider::for_param(&params.tuning, setter));
-
-                            ui.label("Volume");
-                            ui.add(widgets::ParamSlider::for_param(
-                                &params.output_level,
-                                setter,
-                            ));
-                        })
-                });
+                rx11_egui_ui(&egui_state, egui_ctx, setter, state, &params, &presets, &logger);
             },
         )
     }
